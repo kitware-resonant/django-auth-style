82d86561dfb1ceca8d7a63d551b69f6125fa7e48
Brian Helba <brian.helba@kitware.com>
2022-02-09 20:18:58 -0500

    Rename package django-girder-style -> django-auth-style

---
+++ auth_style/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AuthStyleConfig(AppConfig):
+    name = 'auth_style'
+    verbose_name = 'Auth Style'
--- girder_style/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GirderStyleConfig(AppConfig):
-    name = 'girder_style'
-    verbose_name = 'Girder Style'
