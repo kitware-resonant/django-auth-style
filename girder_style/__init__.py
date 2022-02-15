96a095446d2fafb5dfb89c9815568fd6b2329e47
Brian Helba <brian.helba@kitware.com>
2022-02-15 17:27:40 -0500

    Revert "Add deprecation notice for django-girder-style"

--- girder_style/__init__.py
+++ girder_style/__init__.py
@@ -1,12 +1,4 @@
-import warnings
-
 import django
 
 if django.VERSION < (3, 2):
     default_app_config = 'girder_style.apps.GirderStyleConfig'
-
-
-warnings.warn(
-    'The django-girder-style package is deprecated; it has been superseded by django-auth-style.',
-    DeprecationWarning,
-)
