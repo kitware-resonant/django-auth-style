55550765d1364bc08df6aa83318f32ae5b8153e3
Brianna Major <brianna.major@kitware.com>
2025-07-29 16:12:12 -0400

    Add template test to confirm we override the auth_style template

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,4 +1,5 @@
-from django.test import Client
+from django.conf import settings
+from django.test import Client, override_settings
 from django.urls import reverse
 import pytest
 from pytest_django.asserts import assertTemplateUsed
@@ -8,3 +9,21 @@ from pytest_django.asserts import assertTemplateUsed
 def test_template_base_used(client: Client):
     with assertTemplateUsed("auth_style/base.html", count=1):
         client.get(reverse("account_login"))
+
+
+@pytest.mark.django_db
+@override_settings(
+    INSTALLED_APPS=[
+        "test_override_app.auth_style_design",
+        "auth_style",
+        *[
+            app
+            for app in settings.INSTALLED_APPS
+            if app not in {"test_override_app.auth_style_design", "auth_style"}
+        ],
+    ]
+)
+def test_template_override_applied(client: Client):
+    with assertTemplateUsed("auth_style/site_name.html", count=1):
+        response = client.get(reverse("account_login"))
+    assert "🚀 My Custom App" in response.content.decode()
