46c8dccbd4199e60cc29f6f3b5b9a6d7dd6f454d
Brian Helba <brian.helba@kitware.com>
2026-01-06 09:02:20 -0500

    Add support for Django 6.0
    Require Python >=3.12, Django >=5.2.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Callable, Literal
+from collections.abc import Callable
+from typing import TYPE_CHECKING, Literal
 
 from django.conf import settings
 from django.test import Client
